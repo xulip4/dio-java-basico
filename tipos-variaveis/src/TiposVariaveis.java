ab761700dee26b345fce8df6a28ef027b3a02797
xulip4 <raphaelgidrao@hotmail.com>
2023-05-06 12:49:36 -0400

    tipos e variaveis conteudo a estudar

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //estudar tipos primitivos e String
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        final double VALOR_DE_PI = 3.14;
+
+        String nome = "Raphael";
+        int idade = 25;
+        double peso = 63.4;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        Date dataNascimento = new Date();
+        
+        
+
     }
 }
